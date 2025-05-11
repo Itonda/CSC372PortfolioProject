32fcfd94d47c7b67dba6b6449d23f88f3ecc84e3
Itonda <anthellic@gmail.com>
2025-05-11 17:24:29 -0600

    StudentListMenu now uses try with resources for scanner

--- src/main/java/StudentListMenu.java
+++ src/main/java/StudentListMenu.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 /* This class handles the menu for the student list
  * It provides options to add, remove, print, sort, and export the student list */
 public class StudentListMenu {
-    private Scanner scanner = new Scanner(System.in);
     private StudentList studentList = new StudentList();
     private InputStudent inputStudent = new InputStudent();
     private int selection = 0;
@@ -33,7 +32,7 @@ public class StudentListMenu {
     }
     /* This method gets the user's selection from the menu
      * It reads the input and stores it in the selection variable */
-    public void getSelection() {
+    public void getSelection(Scanner scanner) {
         System.out.print("\nEnter your selection: ");
 
         selection = scanner.nextInt(); // Read the user's selection
@@ -41,7 +40,7 @@ public class StudentListMenu {
     }
     /* Helper method for yes/no confirmation 
      * @param prompt The prompt to display to the user */
-    private boolean getYesNoConfirmation(String prompt) throws InvalidStudentException {
+    private boolean getYesNoConfirmation(String prompt, Scanner scanner) throws InvalidStudentException {
         System.out.print(prompt);
         String confirmation = scanner.nextLine().trim(); // Read user input and trim whitespace
 
@@ -57,26 +56,26 @@ public class StudentListMenu {
      * It displays the student ID and name of the student to be removed
      * and asks the user to confirm the removal
      * @param student The student to be removed */
-    private boolean confirmRemoveStudent (Student student) throws InvalidStudentException {
+    private boolean confirmRemoveStudent (Student student, Scanner scanner) throws InvalidStudentException {
         System.out.println("\nAre you sure you want to remove the following student?");
         System.out.println("ID: " + student.getStudentID());
         System.out.println("Name: " + student.getName());
 
-        return getYesNoConfirmation("Enter 'yes' to confirm or 'no' to cancel: ");
+        return getYesNoConfirmation("Enter 'yes' to confirm or 'no' to cancel: ", scanner);
     }
     /* This method confirms if a user wants to add a student
      * Since the studentList.addToList method does not provide an escape
      * give the user a chance to exit switch case 1 */
-    private boolean confirmAddStudent () throws InvalidStudentException {
-        return getYesNoConfirmation("\nAre you sure you want to add a student? (yes/no): ");
+    private boolean confirmAddStudent (Scanner scanner) throws InvalidStudentException {
+        return getYesNoConfirmation("\nAre you sure you want to add a student? (yes/no): ", scanner);
     }
     /* This method executes the sort menu selection
      * It sorts the student list based on the user's choice */
-    public void executeSortMenuSelection() throws InvalidStudentException {
+    public void executeSortMenuSelection(Scanner scanner) throws InvalidStudentException {
         while (true) { // Loop until the user chooses to exit
             try {
                 displaySortMenu(); // Display the sort menu
-                getSelection(); // Get the user's selection
+                getSelection(scanner); // Get the user's selection
                 
                 switch (selection) { // Check the user's selection and execute the corresponding action
                     case 1: // Selection 1: Sort by name ascending
@@ -116,72 +115,74 @@ public class StudentListMenu {
         }
     }
     public void executeMainMenuSelection() throws InvalidStudentException {
-        while (true) { // Loop until the user chooses to exit
-            try {
-                displayMainMenu(); // Display the menu and get the user's selection
-                getSelection(); // Get the user's selection
+        try (Scanner scanner = new Scanner(System.in)) { // Create a new Scanner object for user input
+            while (true) { // Loop until the user chooses to exit
+                try {
+                    displayMainMenu(); // Display the menu and get the user's selection
+                    getSelection(scanner); // Get the user's selection
 
-                switch (selection) { // Check the user's selection and execute the corresponding action
-                    case 1: // Selection 1: Add Student
-                        if (confirmAddStudent()) { // Confirm if the user wants to add a student
-                            studentList.addToList(inputStudent.getStudentFromUser("\nEnter student name: ", 
-                                                                            "Enter student address: ", 
-                                                                            "Enter student GPA: ", 
-                                                                            scanner)); // Get student details from user
-                            System.out.println("\nStudent added successfully.");
-                        break;
-                        } else {
-                            System.out.println("\nStudent addition canceled."); // If the user does not confirm, exit the case
+                    switch (selection) { // Check the user's selection and execute the corresponding action
+                        case 1: // Selection 1: Add Student
+                            if (confirmAddStudent(scanner)) { // Confirm if the user wants to add a student
+                                studentList.addToList(inputStudent.getStudentFromUser("\nEnter student name: ", 
+                                                                                "Enter student address: ", 
+                                                                                "Enter student GPA: ", 
+                                                                                scanner)); // Get student details from user
+                                System.out.println("\nStudent added successfully.");
                             break;
-                        }
-                    case 2: // Selection 2: Remove Student - Provide exit condition (0)
-                        int studentId = inputStudent.getStudentIdFromUser("\nEnter student ID to remove (enter 0 for main menu): ", 
-                                                                            scanner); // Get the student ID or exit input from user
-
-                        if (studentId == 0) { // Check for exit condition
-                            System.out.println("\nExiting to main menu.");
-                            break; // If exit condition: exit to main menu
-                        }
-                        
-                        int index = studentList.searchById(studentId); // Search for the student by ID and assign the index
-
-                        if (index != -1) { // If the student is found
-                            Student studentToRemove = studentList.getStudentList().get(index); // Get the student object    
-                            if (confirmRemoveStudent(studentToRemove)) { // Confirm removal
-                                studentList.removeStudentByID(studentId); // Remove the student from the list
-                                System.out.println("\nStudent removed successfully.");
                             } else {
-                                System.out.println("\nStudent removal canceled.");
+                                System.out.println("\nStudent addition canceled."); // If the user does not confirm, exit the case
+                                break;
                             }
-                        } else {
-                            System.out.println("\nStudent not found."); // If not found, display not found message
-                        }
-                        break;  
-                    case 3: // Selection 3: Print Student List
-                        if (studentList.isEmpty()) { // Check if the list is empty
-                            System.out.println("\nThe student list is empty."); // If empty, display message
-                        } else {
-                            System.out.println("\nStudent List:"); // If not empty, display the student list
-                        }
-                        studentList.printList(); 
-                        break;
-                    case 4: // Selection 4: Sort Student List
-                        executeSortMenuSelection(); // Call the method to execute sort menu selection
-                        break;
-                    case 5: // Selection 5: Export Student List
-                        System.out.println("\nExporting the student list...");
-                        ExportToFile.exportToTextFile(studentList.toString(), "studentList.txt"); // Use the ExportStudentList class to export the list
-                        break;
-                    case 6: // Selection 6: Exit
-                        System.out.println("\nExiting the program.");
-                        return; // Exit the program
-                    default:
-                        System.out.println("\nInvalid selection. Please try again."); // If invalid selection, display message
-                }  
-            } catch (InputMismatchException e) { // Catch any input mismatch exceptions
-                System.out.println("\nInvalid input. Please enter a number.");
-                scanner.nextLine(); // Clear the invalid input
+                        case 2: // Selection 2: Remove Student - Provide exit condition (0)
+                            int studentId = inputStudent.getStudentIdFromUser("\nEnter student ID to remove (enter 0 for main menu): ", 
+                                                                                scanner); // Get the student ID or exit input from user
+
+                            if (studentId == 0) { // Check for exit condition
+                                System.out.println("\nExiting to main menu.");
+                                break; // If exit condition: exit to main menu
+                            }
+                            
+                            int index = studentList.searchById(studentId); // Search for the student by ID and assign the index
+
+                            if (index != -1) { // If the student is found
+                                Student studentToRemove = studentList.getStudentList().get(index); // Get the student object    
+                                if (confirmRemoveStudent(studentToRemove, scanner)) { // Confirm removal
+                                    studentList.removeStudentByID(studentId); // Remove the student from the list
+                                    System.out.println("\nStudent removed successfully.");
+                                } else {
+                                    System.out.println("\nStudent removal canceled.");
+                                }
+                            } else {
+                                System.out.println("\nStudent not found."); // If not found, display not found message
+                            }
+                            break;  
+                        case 3: // Selection 3: Print Student List
+                            if (studentList.isEmpty()) { // Check if the list is empty
+                                System.out.println("\nThe student list is empty."); // If empty, display message
+                            } else {
+                                System.out.println("\nStudent List:"); // If not empty, display the student list
+                            }
+                            studentList.printList(); 
+                            break;
+                        case 4: // Selection 4: Sort Student List
+                            executeSortMenuSelection(scanner); // Call the method to execute sort menu selection
+                            break;
+                        case 5: // Selection 5: Export Student List
+                            System.out.println("\nExporting the student list...");
+                            ExportToFile.exportToTextFile(studentList.toString(), "studentList.txt"); // Use the ExportStudentList class to export the list
+                            break;
+                        case 6: // Selection 6: Exit
+                            System.out.println("\nExiting the program.");
+                            return; // Exit the program
+                        default:
+                            System.out.println("\nInvalid selection. Please try again."); // If invalid selection, display message
+                    }  
+                } catch (InputMismatchException e) { // Catch any input mismatch exceptions
+                    System.out.println("\nInvalid input. Please enter a number.");
+                    scanner.nextLine(); // Clear the invalid input
+                }
             }
-        }
-    }   
+        }   
+    }
 }
